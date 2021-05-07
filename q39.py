30972bb5dba2653c1fbe6cb73d79484fba3c441a
osama1998H <osamamuhammed555@gmail.com>
2021-05-07 14:48:17 +0300

    little changes

--- q39.py
+++ q39.py
@@ -1,18 +1,43 @@
+from math import sqrt
+import numpy as np
 x1, y1, x2, y2, x3, y3 = map(float, input(
     "enter x1 y1 x2 y2 x3 y3 values: ").split())
 print(x1, y1, x2, y2, x3, y3)
 
-a = (x1*(y2-y3) - y1*(x2 - x3) + (x2*y3) - (x3*y2))
-b = (x1**2 + y1**2) * (y3-y2) + (x2**2 + y2**2)* (y1 - y3)+ (x3**2 + y3**2)* (y2 - y1)
-c = (x1**2 + y1**2) * (x2-x3) + (x2**2 + y2**2)* (x3 - x1)+ (x3**2 + y3**2)* (x1 - x2)
-d = (x1**2 + y1**2) * (x3*y2 - x2 *y3) + (x2**2 + y2**2) * (x1*y3 - x3 *y1) + (x3**2 + y3**2) * (x2*y1 - x1 *y3)
-x = -(b/ (2*a))
-y = -(c/(2*a))
-from math import sqrt
+a = np.array([[x1, x2, x3], [y1, y2, y3], [1, 1, 1]])
 
-r = sqrt(((b**2) + (c**2) - (4*a*d))/ 4*(a**2))
+b = -np.array([
+    [x1**2 + y1**2, x2**2 + y2**2, x3**2 + y3**2],
+    [y1, y2, y3],
+    [1, 1, 1]
+])
+c = np.array([
+    [x1**2 + y1**2, x2**2 + y2**2, x3**2 + y3**2],
+    [x1, x2, x3],
+    [1, 1, 1]
+])
+d = -np.array([
+    [x1**2 + y1**2, x2**2 + y2**2, x3**2 + y3**2],
+    [x1, x2, x3],
+    [y1, y2, y3]
+])
 
-print(f"radios: {sqrt(r)}")
-print(f"x, y: {x}, {y}")
-# print(f"radios: ")
-# print(f"radios: ")
+
+a = np.linalg.det(a)
+b = np.linalg.det(b)
+c = np.linalg.det(c)
+d = np.linalg.det(d)
+
+x = round(-(b/(2*a)), 3)
+y = round(-(c/(2*a)), 3)
+
+a_s = a**2
+b_s = b**2
+c_s = c**2
+
+
+
+r = sqrt(((b_s + c_s / 4 * a_s) - d / a))
+
+
+print(x, y, sqrt(r))
